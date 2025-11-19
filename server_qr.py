c538ed5cdfd43e30cf10a63987d0a849fb66e912
kevinhuang02 <enjoyher2004@gmail.com>
2025-11-19 11:28:14 +0800

    Add files via upload

--- server_qr.py
+++ server_qr.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddlewareimport 
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 import uvicorn
 import os
